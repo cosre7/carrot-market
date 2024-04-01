daead16a546603ed8bc02e59208c2e7abb357c61
cosre7 <cosre7@gmail.com>
2024-04-01 23:11:49 +0900

    [feat] 리스트에서 짝수와 홀수옵션

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
-        <input className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus: ring-offset-2 transition-shadow placeholder:drop-shadow-lg invalid:focus:ring-red-500 peer" required type="text" placeholder="Email address"/>
-        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required.</span>
-        <button className="bg-black text-white py-2 rounded-full active:scale-90  transition-transform font-medium md:px-10 peer-required:bg-green-500">Log In</button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => 
+          (<div key={index} className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl">
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>)
+        )}
       </div>
     </main>
   );
