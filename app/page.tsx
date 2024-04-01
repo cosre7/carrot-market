262853ca5226351010a04130c77343c393ab1b0d
cosre7 <cosre7@gmail.com>
2024-04-01 22:38:34 +0900

    [feat] 그라데이션 버튼 만들기

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus: ring-offset-2 transition-shadow placeholder:drop-shadow-lg" type="text" placeholder="Search here..."/>
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90  transition-transform font-medium outline-none md:px-10">Search</button>
+        <button className="bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400 bg-opacity-50 text-white py-2 rounded-full active:scale-90  transition-transform font-medium outline-none md:px-10">Search</button>
       </div>
     </main>
   );
