42bbb99aa48f9b0d4c49d1a75ecd4b5e37195570
cosre7 <cosre7@gmail.com>
2024-04-03 22:07:08 +0900

    [feat] nofitication에 활용하기 좋은 ping 애니메이션

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => 
-          (<div key={index} className="flex items-center gap-5 *:animate-pulse">
+          (<div key={index} className="flex items-center gap-5">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <div className="w-40 h-4 rounded-full bg-gray-400"></div>
-            <div className="w-20 h-4 rounded-full bg-gray-400"></div>
-            {/* <span className="text-lg font-medium">{person}</span> */}
-            {/* <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index}</span>
-            </div> */}
+            <span className="text-lg font-medium">{person}</span>
+            <div className="relative size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
+            </div>
           </div>)
         )}
       </div>
