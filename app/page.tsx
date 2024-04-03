170345a9e0bd36ca31b95eda7a4c497ff0f8a57e
cosre7 <cosre7@gmail.com>
2024-04-03 21:57:00 +0900

    [feat] last, fist 요소 선택

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => 
-          (<div key={index} className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl">
+          (<div key={index} className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0">
             <div className="size-10 bg-blue-400 rounded-full" />
             <span className="text-lg font-medium">{person}</span>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
